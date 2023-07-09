d0dc8d59b73f12fcce1698e61fb65ca037204a32
Kedar Pandya <72641311+kedarrpandya@users.noreply.github.com>
2023-07-09 09:34:04 +0530

    Add files via upload

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -16,8 +16,11 @@ const Hero = (props) => (
         My Personal Portfolio
       </SectionTitle>
       <SectionText>
-        I am Kedar Pandya. I'm currently a 4th year student of B. Tech.
-        Information & Communication Technology at Ganpat University.
+        I am Kedar Pandya. I have done my Bachelors of Technology in Information
+        and Communication Technology from Ganpat University, Kherva, India. I am
+        currently doing my job as a Customer Success Engineer in
+        <b>Trend Micro Inc., India</b>. I am handling an Endpoint Security
+        product.
       </SectionText>
       <Button onClick={() => (window.location = "#Projects")}>
         Learn More
